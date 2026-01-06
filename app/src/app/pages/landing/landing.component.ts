4784e45cdc44fe05532c0fd74a79a379774e022c
cesarvillajo-dev <cesarvillajo.dev@gmail.com>
2026-01-06 08:59:54 +0100

    ajustes media

--- app/src/app/pages/landing/landing.component.ts
+++ app/src/app/pages/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {}
+export class LandingComponent {
+
+constructor(private router: Router) {}
+
+goToComponents(): void {
+  this.router.navigateByUrl('/components');
+  
+}
+
+scrollTo(id: string) {
+  const el = document.getElementById(id);
+  if (el) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+}
